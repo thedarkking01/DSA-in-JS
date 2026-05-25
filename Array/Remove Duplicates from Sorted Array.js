1e1499edce8747bb50d7148f223217a97ac34a3e
thedarkking01 <sourabhsinghbais52@gmail.com>
2026-05-25 21:58:27 +0530

    done some array problem

---
+++ Array/Remove Duplicates from Sorted Array.js
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let x=0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]>nums[x]){
+            x=x+1;
+            nums[x]=nums[i]
+        }
+    }
+    return (x+1)
+};
