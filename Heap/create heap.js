9451f1c966efebcd0e8f20b76cd20ded570141cc
thedarkking01 <sourabhsinghbais52@gmail.com>
2026-06-30 19:08:32 +0530

    done heapify down

--- Heap/create heap.js
+++ Heap/create heap.js
@@ -28,8 +28,46 @@ class Minheap {
             }
         }
     }
+
+    extract() {
+        if (this.heap.length === 0) {
+            return null
+        }
+        let min = this.heap[0]
+        let lastIndex = this.heap.length - 1;
+        [this.heap[0], this.heap[lastIndex]] = [this.heap[lastIndex], this.heap[0]]
+        this.heap.pop()
+        this.heapifyDown(0)
+        return min
+    }
+
+    heapifyDown(i) {
+        let left = this.getLeftChildIndex(i)
+        let right = this.getRightChildIndex(i)
+        let smallest = i
+        if (left < this.heap.length && this.heap[left] < this.heap[smallest]) {
+            smallest = left
+        }
+        if (right < this.heap.length && this.heap[right] < this.heap[smallest]) {
+            smallest = right
+        }
+        if (smallest !== i) {
+            [this.heap[i], this.heap[smallest]] = [this.heap[smallest], this.heap[i]]
+            this.heapifyDown(smallest)
+        }
+    }
+
+    peek() {
+        if (this.heap.length === 0) {
+            return null
+        }
+        return this.heap[0]
+    }
 }
 
 let heap = new Minheap()
 heap.insert(1)
 console.log(heap)
+console.log(heap.extract())
+console.log(heap)
+console.log(heap.extract())
